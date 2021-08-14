e76f26acdfbb32ce001fa841ac2d6475a20b67f6
revati1628 <88822829+revati1628@users.noreply.github.com>
2021-08-14 22:15:50 +0530

    Add files via upload

---
+++ RequestsbyUser.js
@@ -0,0 +1,57 @@
+import React,{useEffect,useState} from 'react'
+import axios from 'axios';
+export default function RequestsbyUser() {
+
+    const [issueList,setIssueList]= useState([])
+    useEffect(() => {
+        getIssuesList();
+        
+   }, [])
+
+   const getIssuesList= async() =>{
+       const result=await axios.get("http://localhost:8080/getIssues");
+       setIssueList(result.data);
+      
+   }
+   //const list=issueList;
+   //console.log("first",list[0]);
+   console.log("issue list",issueList);
+   
+    return (
+        <div style={{paddingTop:"20px"}}>
+       
+              <h1 >Request IDs</h1> 
+                
+                {
+                    issueList.map((list,ind)=>{
+                       const response=()=>{
+                            console.log(list.issue);
+                            console.log(list.description);
+                            console.log(list.date);
+                       }
+                        return(
+                            <div key={ind}>
+                                
+                                <button type="button" style={{color:"green"}} onClick={response}  >{list.requestId}
+                                
+                               
+
+                                </button>
+                                
+                                
+                               
+                            </div>
+                        )
+                    })
+                }
+           
+            
+                
+                
+       
+         
+
+         </div>
+    
+    )
+}
