2301bedf7401680317fe01980fa780ea49637c6a
revati1628 <88822829+revati1628@users.noreply.github.com>
2021-08-16 23:32:37 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {useState} from "react";
+import Routing from './components/Routing'
+function App() {
+
+  
+  return(
+    <div>
+      <Routing /> 
+    </div>
+  )
+}
+export default App;
