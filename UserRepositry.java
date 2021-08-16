6dae755afb507b206933acb34d68663883fcbeba
revati1628 <88822829+revati1628@users.noreply.github.com>
2021-08-16 23:42:23 +0530

    Update UserRepositry.java

--- UserRepositry.java
+++ UserRepositry.java
@@ -1,19 +1,31 @@
-package com.security.app.repo;
+package com.app.project.dao;
 
-
+import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
-import com.security.app.model.User;
+import com.app.project.model.User;
 
-@Service
+
+//@Service
 @Repository
-public interface UserRepositry extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User,Integer> {
 
+	@Transactional
+	@Modifying
+	@Query("update User u set u.password=:password where u.id=:id")
+    public void updatePassword(int id,String password);
+	
 	User findByPassword(String password);
+	
+	
 
-//	User findById(long id);
+//		User findById(long id);
 
+	
 }
+
