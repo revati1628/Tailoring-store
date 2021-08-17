121f564e7b132e950aab55e002278081026b932c
revati1628 <88822829+revati1628@users.noreply.github.com>
2021-08-17 15:47:37 +0530

    Update SecurityQuestion.java

--- SecurityQuestion.java
+++ SecurityQuestion.java
@@ -11,11 +11,11 @@ public class SecurityQuestion {
 	@Id
 	//@GeneratedValue(strategy = GenerationType.AUTO)
 	private int userId;
-	private String securityquestion1;
+	
 	private String securityanswer1;
-	private String securityquestion2;
+	
 	private String securityanswer2;
-	private String securityquestion3;
+	
 	private String securityanswer3;
 	private String contact;
 	
@@ -23,15 +23,15 @@ public class SecurityQuestion {
 		super();
 	}
 
-	public SecurityQuestion(int userId, String securityquestion1, String securityanswer1, String securityquestion2,
-			String securityanswer2, String securityquestion3, String securityanswer3, String contact) {
+	public SecurityQuestion(int userId, String securityanswer1,
+			String securityanswer2,  String securityanswer3, String contact) {
 		super();
 		this.userId = userId;
-		this.securityquestion1 = securityquestion1;
+		
 		this.securityanswer1 = securityanswer1;
-		this.securityquestion2 = securityquestion2;
+		
 		this.securityanswer2 = securityanswer2;
-		this.securityquestion3 = securityquestion3;
+		
 		this.securityanswer3 = securityanswer3;
 		this.contact = contact;
 	}
@@ -44,13 +44,7 @@ public class SecurityQuestion {
 		this.userId = userId;
 	}
 
-	public String getSecurityquestion1() {
-		return securityquestion1;
-	}
-
-	public void setSecurityquestion1(String securityquestion1) {
-		this.securityquestion1 = securityquestion1;
-	}
+	
 
 	public String getSecurityanswer1() {
 		return securityanswer1;
@@ -60,13 +54,7 @@ public class SecurityQuestion {
 		this.securityanswer1 = securityanswer1;
 	}
 
-	public String getSecurityquestion2() {
-		return securityquestion2;
-	}
-
-	public void setSecurityquestion2(String securityquestion2) {
-		this.securityquestion2 = securityquestion2;
-	}
+	
 
 	public String getSecurityanswer2() {
 		return securityanswer2;
@@ -76,13 +64,7 @@ public class SecurityQuestion {
 		this.securityanswer2 = securityanswer2;
 	}
 
-	public String getSecurityquestion3() {
-		return securityquestion3;
-	}
-
-	public void setSecurityquestion3(String securityquestion3) {
-		this.securityquestion3 = securityquestion3;
-	}
+	
 
 	public String getSecurityanswer3() {
 		return securityanswer3;
@@ -102,11 +84,11 @@ public class SecurityQuestion {
 
 	@Override
 	public String toString() {
-		return "SecurityQuestion [userId=" + userId + ", securityquestion1=" + securityquestion1 + ", securityanswer1="
-				+ securityanswer1 + ", securityquestion2=" + securityquestion2 + ", securityanswer2=" + securityanswer2
-				+ ", securityquestion3=" + securityquestion3 + ", securityanswer3=" + securityanswer3 + ", contact="
-				+ contact + "]";
+		return "SecurityQuestion [userId=" + userId + ", securityanswer1=" + securityanswer1 + ", securityanswer2="
+				+ securityanswer2 + ", securityanswer3=" + securityanswer3 + ", contact=" + contact + "]";
 	}
+
+	
 	
 	
 
