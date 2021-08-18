0cf7513c1e39d9e01a8e23765fae17318de79ea7
revati1628 <88822829+revati1628@users.noreply.github.com>
2021-08-18 18:05:16 +0530

    Update AdminPage.js

--- AdminPage.js
+++ AdminPage.js
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import IssuesNotification from './IsuuesNotification'
+import SortByAdmin from './SortByAdmin'
+import {Link,useHistory} from "react-router-dom";
 export default function AdminPage() {
+
+    let history=useHistory();
     return (
         <div>
             <h1>Admin page</h1>
+            <IssuesNotification />
+            <SortByAdmin />
         </div>
     )
+   
 }
