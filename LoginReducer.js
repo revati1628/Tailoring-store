3f3755932445c464c7676ae610e0681acb2d7a1e
revati1628 <88822829+revati1628@users.noreply.github.com>
2021-08-18 16:28:24 +0530

    Update LoginReducer.js

--- LoginReducer.js
+++ LoginReducer.js
@@ -1,4 +1,8 @@
-const initialState={id:' ',firstname:' ',lastname:'',dob:'',usercategory:'',gender:'',password:' ',contact:' ',email:' ',getusers:[],post:[],validate:[]}
+const initialState={id:' ',firstname:' ',lastname:'',dob:'',usercategory:'',gender:'',password:' ',contact:' ',email:' ',
+                    orderid:' ',rate:'',improve:'',likes:'',
+                    sortid:' ',category:' ',dresstype:' ',occassion:'',
+                    data:'',
+                    getusers:[],post:[],validate:[]}
 const loginreducer=(state=initialState,action)=>{
     switch(action.type){
         case "id":return{...state,id:action.payload}
@@ -10,6 +14,19 @@ const loginreducer=(state=initialState,action)=>{
         case "password":return{...state,password:action.payload}
         case "contact":return{...state,contact:action.payload}
         case "email":return{...state,email:action.payload}
+
+        case "orderid":return{...state,orderid:action.payload}
+        case "improve":return{...state,improve:action.payload}
+        case "likes":return{...state,likes:action.payload}
+        case "rate":return{...state,rate:action.payload}
+
+        case "data":return{...state,data:action.payload}
+
+        case "sortid":return{...state,sortid:action.payload}
+        case "category":return{...state,category:action.payload}
+        case "dresstype":return{...state,dresstype:action.payload}
+        case "occassion":return{...state,occassion:action.payload}
+
         case "getusers":return{...state,getusers:action.payload}
         case "post":return{...state,post:action.payload}
         case "validate":return{...state,validate:action.payload}
