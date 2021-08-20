3afda5b01b8883e79d4000c5efe7b56cb751c25a
revati1628 <88822829+revati1628@users.noreply.github.com>
2021-08-20 13:24:18 +0530

    Create TailorService.java

---
+++ TailorService.java
@@ -0,0 +1,34 @@
+package com.app.project.service;
+
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.app.project.dao.TailorRepository;
+
+import com.app.project.model.Tailor;
+
+
+@Service
+public class TailorService {
+
+	@Autowired
+	TailorRepository tailorrepository;
+	
+	public Tailor storedata(Tailor tailor) {
+		tailorrepository.save(tailor);
+		return tailor;
+	}
+	
+	public List<Tailor> getdata(){
+		List< Tailor> tailor=tailorrepository.findAll();
+		return tailor;
+	}
+	
+	public void updateData(Tailor tailor) {
+		// TODO Auto-generated method stub
+		tailorrepository.save(tailor);
+		}
+}
