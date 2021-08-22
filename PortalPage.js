012b32284f5c1a46c921a312609d2b184e3d05b0
revati1628 <88822829+revati1628@users.noreply.github.com>
2021-08-22 22:11:10 +0530

    Add files via upload

--- PortalPage.js
+++ PortalPage.js
@@ -1,64 +1,53 @@
 import React from 'react'
-import SolutionsForm from '../AdminComponents/SolutionsForm';
-import './Portal.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card'
+import '../index.css';
  function PortalPage() {
     return (
     <div className="faq">
-   <h1>Frequently asked Queries</h1>
- 
-<h4>What is mode of payment?</h4>
-<select id = "dropdown">
-    <option value="Answers">Answers</option>
-    <option value="1">Payment is done through netbanking </option>
-    <option value="2">Payment is done through COD</option>
-    </select>
- 
-<h4>Do you have return policies?</h4>
-<select id = "dropdown">
-    <option value="Answers">Answers</option>
-    <option value="1"> yes,we have return policy within 2weeks you have to return. </option>
-    <option value="2">Return policy is accepted only within the given time.</option>
-    </select>
- 
-<h4>What time will the goods be shipped? </h4>
-<select id = "dropdown">
-    <option value="Answers">Answers</option>
-    <option value="1"> Our goods will be shipped on time</option>
-    </select>
- 
-<h4>How standard are the products you are selling?</h4>
-<select id = "dropdown">
-    <option value="Answers">Answers</option>
-    <option value="1">  Our products are best and we will use best fabrics. </option>
-    </select>
- 
-<h4>Is the site safe?</h4>
-<select id = "dropdown">
-    <option value="Answers">Answers</option>
-    <option value="1"> yes,it is 100percent safe.you can trust us. </option>
-    </select>
- 
-<h4>What are your customer reviews look like?</h4>
-<select id = "dropdown">
-    <option value="Answers">Answers</option>
-    <option value="1"> This is very important before making any payment online. You need to confirm that they have legitimate customer reviews. </option>
-    </select>
- 
-<h4>How can i track my shipment?</h4>
-<select id = "dropdown">
-    <option value="Answers">Answers</option>
-    <option value="1">  You can track your shipment by clicking on status of product. </option>
-    </select>
- 
-<h4>Do you know anyone who has shopped from this site?</h4>
-<select id = "dropdown">
-    <option value="Answers">Answers</option>
-    <option value="1"> You simply need to ask the people you know about how the site operates. Their honest opinion will help you ascertain the site’s quality.  </option>
-    </select>
-    <br></br>
-    <br/>
-    <SolutionsForm />
-   </div>
- )
+    
+   
+   <h2 style={{color:'black',marginTop:'20px',textAlign:'center'}}>Frequently Asked Queries</h2>
+   
+<Card>
+  <Card.Body>
+    <blockquote className="blockquote mb-0">
+     <ul>
+
+      <li><b>What to do if i gave wrong measurements?</b></li>
+       <h6>You can update your requests and intimate to tailor.</h6>
+
+        <li><b> What is mode of payment?</b></li>
+       <h6>Payment is done through netbanking and COD.</h6>
+       
+       <li><b>Do you have return policies?</b></li>
+       <h6>yes,we have return policy within 2weeks you have to return.</h6>
+       <h6>Return policy is accepted only within the given time.</h6>
+
+        <li><b> What time will the goods be shipped? </b></li>
+       <h6>Our goods will be shipped on time</h6>
+
+        <li><b> How can i track my shipment?</b></li>
+       <h6>You can track your shipment by clicking on status of product.</h6>
+
+      <li><b>At trasaction process i had some  network issues what happens? </b></li>
+       <h6>It simply goes to the page where you have stopped.</h6>
+
+       <li><b> At the time of delivery if i was not available what to do?</b></li>
+       <h6>You need to inform and tell the reason to the authorized person.</h6>
+
+        <li><b> What happens when application suddenly stops?</b></li>
+       <h6>Mostly it wont but if happens please use after some time its because of technical glitches.</h6>
+       </ul> 
+      
+      
+    </blockquote>
+  </Card.Body>
+</Card>
+  
+</div>
+    
+     
+       )
 }
 export default PortalPage;
