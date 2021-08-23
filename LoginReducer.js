c2fe5884ceaffa277623b4ec16b6671595439bba
revati1628 <88822829+revati1628@users.noreply.github.com>
2021-08-23 13:39:21 +0530

    Update LoginReducer.js

--- LoginReducer.js
+++ LoginReducer.js
@@ -2,6 +2,8 @@ const initialState={id:' ',firstname:' ',lastname:'',dob:'',usercategory:'',gend
                     orderid:' ',rate:'',improve:'',likes:'',
                     sortid:' ',category:' ',dresstype:' ',occassion:'',
                     data:'',
+                    topfabric:'',topduration:'',topwaist:'',toplength:'',chest:'',shoulderlength:'',topamount:'',
+                    neck:'',bottomfabric:'',bottomduration:'',bottomamount:'',kneelength:'',hip:'',comments:'',
                     getusers:[],post:[],validate:[]}
 const loginreducer=(state=initialState,action)=>{
     switch(action.type){
@@ -26,6 +28,21 @@ const loginreducer=(state=initialState,action)=>{
         case "category":return{...state,category:action.payload}
         case "dresstype":return{...state,dresstype:action.payload}
         case "occassion":return{...state,occassion:action.payload}
+        
+         case "topfabric":return{...state,topfabric:action.payload}
+        case "topduration":return{...state,topduration:action.payload}
+        case "topwaist":return{...state,topwaist:action.payload}
+        case "toplength":return{...state,toplength:action.payload}
+        case "chest":return{...state,chest:action.payload}
+        case "shoulderlength":return{...state,shoulderlength:action.payload}
+        case "topamount":return{...state,topamount:action.payload}
+        case "neck":return{...state,neck:action.payload}
+        case "bottomfabric":return{...state,bottomfabric:action.payload}
+        case "bottomduration":return{...state,bottomduration:action.payload}
+        case "bottomamount":return{...state,bottomamount:action.payload}
+        case "kneelength":return{...state,kneelength:action.payload}
+        case "hip":return{...state,hip:action.payload}
+        case "comments":return{...state,comments:action.payload}
 
         case "getusers":return{...state,getusers:action.payload}
         case "post":return{...state,post:action.payload}
