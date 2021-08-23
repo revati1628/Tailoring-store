c66b8435108b9ad10ffa66730421854c89001632
revati1628 <88822829+revati1628@users.noreply.github.com>
2021-08-23 14:30:49 +0530

    Create ResponseMsg.java

---
+++ ResponseMsg.java
@@ -0,0 +1,22 @@
+package com.app.project.ResponseMsg;
+
+public class ResponseMsg {
+
+	
+		  private String message;
+
+		  public ResponseMsg(String message) {
+		    this.message = message;
+		  }
+
+		  public String getMessage() {
+		    return message;
+		  }
+
+		  public void setMessage(String message) {
+		    this.message = message;
+		  }
+
+		
+}
+
