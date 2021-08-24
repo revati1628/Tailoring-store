6f8bba4743424cb60797be936c2be4a039d8fdc0
revati1628 <88822829+revati1628@users.noreply.github.com>
2021-08-24 23:59:50 +0530

    Update Routing.js

--- Routing.js
+++ Routing.js
@@ -11,7 +11,7 @@ import AdminLogin from './adminLogin';
 import IssuesNotification from '../AdminComponents/IsuuesNotification';
 //import RequestsbyUser from "./AdminComponents/RequestsbyUser";
 import PortalPage from "./PortalPage";
-import FeedbackForm from "../AdminComponents/FeedbackForm";
+//import FeedbackForm from "../AdminComponents/FeedbackForm";
 import SecurityQuestionForm from "./SecurityQuestionForm";
 import ForgotPassword from "../UserComponents/ForgotPassword";
 import ForgotId from "../UserComponents/ForgotId";
@@ -21,7 +21,7 @@ import TailorPage from '../TailorComponents/TailorPage';
 import UpdateDetails from "../TailorComponents/UpdateDetails";
 import TailorSorting from '../TailorComponents/TailorSorting';
 import UploadPatterns from '../TailorComponents/UploadPatterns';
-import OrderDetails from "../CustomerComponents/OrderDetails";
+//import OrderDetails from "../CustomerComponents/OrderDetails";
 import Navbar from '../Styling/Navbar';
 import Description from "../Styling/Description";
 import Footer from "../Styling/Footer";
@@ -30,6 +30,16 @@ import TailorSearch from '../CustomerComponents/TailorSearch';
 import PlaceOrder from '../CustomerComponents/PlaceOrder';
 import ViewOrders from '../AdminComponents/ViewOrders';
 import CustomerOrders from '../AdminComponents/CustomerOrders';
+import ViewOrder from "../TailorComponents/ViewOrder";
+import CheckOrders from '../TailorComponents/CheckOrders';
+import OrderBystatus from '../TailorComponents/OrderBystatus';
+import PayBill from '../CustomerComponents/PayBill';
+import MakePayment from '../CustomerComponents/MakePayment';
+import PaymentForm from '../CustomerComponents/PaymentForm';
+import AdminFeedback from '../AdminComponents/AdminFeedback';
+import ViewByOrder from '../CustomerComponents/ViewByOrder';
+import SuccessPay from '../CustomerComponents/SuccessPay';
+import PaymentStatus from '../TailorComponents/PaymentStatus';
 export default function Routing() {
     return (
        
@@ -61,7 +71,7 @@ export default function Routing() {
         <Route path="/UserPage" component={UserPage}></Route>
 
         <Route path="/HelpPage" component={HelpPage}></Route>
-        <Route path="/FeedbackForm" component={FeedbackForm}></Route>
+        <Route path="/AdminFeedback" component={AdminFeedback}></Route>
         <Route path="/SecurityQuestionForm" component={SecurityQuestionForm}></Route>
         <Route path="/ForgotId" component={ForgotId}></Route>
         <Route path="/ForgotPassword" component={ForgotPassword}></Route>
@@ -71,12 +81,20 @@ export default function Routing() {
         <Route path="/UpdateDetails" component={UpdateDetails}></Route>
         <Route path="/TailorSorting" component={TailorSorting}></Route>
         <Route path="/UploadPatterns" component={UploadPatterns}></Route>
-        <Route path="/OrderDetails" component={OrderDetails}></Route>
+        <Route path="/CheckOrders" component={CheckOrders}></Route>
         <Route path="/SolutionsForm" component={SolutionsForm}></Route>
         <Route path="/TailorSearch" component={TailorSearch}></Route>
         <Route path="/PlaceOrder/:tailorid" component={PlaceOrder}></Route>
         <Route path="/ViewOrders" component={ViewOrders}></Route>
+        <Route path="/ViewOrder" component={ViewOrder}></Route>
         <Route path="/CustomerOrders" component={CustomerOrders}></Route>
+        <Route path="/OrderBystatus" component={OrderBystatus}></Route>
+        <Route path="/PayBill" component={PayBill}></Route>
+        <Route path="/MakePayment" component={MakePayment}></Route>
+        <Route path="/PaymentForm" component={PaymentForm}></Route>
+        <Route path="/ViewByOrder" component={ViewByOrder}></Route>
+        <Route path="/SuccessPay" component={SuccessPay}></Route>
+        <Route path="/PaymentStatus" component={PaymentStatus}></Route>
         
      
       
