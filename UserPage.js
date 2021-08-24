d2455a1928727c91176d71510100d10b2ee01ed9
revati1628 <88822829+revati1628@users.noreply.github.com>
2021-08-24 19:35:20 +0530

    Update UserPage.js

--- UserPage.js
+++ UserPage.js
@@ -1,9 +1,9 @@
 import React,{useState} from 'react'
 import { useHistory,Link, BrowserRouter, Switch,Route } from "react-router-dom";
-import FeedbackForm from "../AdminComponents/FeedbackForm";
 import {AiOutlineSearch} from "react-icons/ai"
 import '../App.css';
 import user from '../Images/user.jpg'
+import AdminFeedback from './AdminFeedback';
 
 export default function UserPage(props) {
 
@@ -21,11 +21,12 @@ export default function UserPage(props) {
     }
 
     return (
+        
         <div style={{backgroundImage:`url(${user})`,height:'600px'}}>
-            <button className="btn btn-primary" onClick={logout} style={{float:"right"}}>LogOut</button>
-            <div style={{paddingRight:"100px",paddingLeft:"500px"}}><button type="button" className="btn btn-outline-info"  onClick={()=>setButtonPopup(true)}>would you like to give us Feedback ?</button>       
+            <button className="btn btn-primary" onClick={()=>setButtonPopup(true)} style={{float:"right"}}>LogOut</button>
+            <div style={{paddingRight:"100px",paddingLeft:"500px"}}><button type="button" className="btn btn-outline-info" >would you like to give us Feedback ?</button>       
 
-            <FeedbackForm trigger={buttonPopup} setTrigger={setButtonPopup}/>
+            <AdminFeedback trigger={buttonPopup} setTrigger={setButtonPopup}/>
             </div>
         <div>
         <nav className="nav-item "  style={{color:"white",fontSize:"35px"}}>
