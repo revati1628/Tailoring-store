6f23358b18d35a812f5a21e71b12600cde21ce13
revati1628 <88822829+revati1628@users.noreply.github.com>
2021-08-24 23:23:38 +0530

    Update UploadPatterns.js

--- UploadPatterns.js
+++ UploadPatterns.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react'
 import { useHistory } from 'react-router-dom';
-export default function ImageUpload() {
+export default function UploadPatterns() {
 
   
   const [selectedFile, setSelectedFile] = useState({
@@ -23,7 +23,7 @@ export default function ImageUpload() {
     
      
 
-      fetch(`http://localhost:8080/upload/${selectedFile.tailorid}`,{
+      fetch(`http://localhost:8090/upload/${selectedFile.tailorid}`,{
         method: 'POST',
         
         body: fd
@@ -38,26 +38,27 @@ export default function ImageUpload() {
   
   return (
     <div>
-       <button type="button" className="btn btn-outline-primary" style={{float:"right",paddingRight:"10px"}}
-            onClick={back}>Back</button>
-     <form onSubmit={handleSubmit} encType="multipart/form-data" id="formele" style={{marginTop:'20px',marginLeft:'450px'}}>
-        Enter tailor id : <input
+       <button type="button" className="btn btn-outline-primary" style={{float:"right",paddingRight:"10px",marginTop:'10px'}}
+            onClick={back}>Back</button><br/>
+     <form onSubmit={handleSubmit} encType="multipart/form-data" id="formele" style={{marginLeft:'450px',}}>
+     <h3  style={{color:'black',marginBottom:'20px'}}>PLEASE FILL THE DETAILS</h3>
+        <b>Enter tailor id</b> <br/> <input
           type="number"  id="tailorid"
           
-          onChange={(e)=>{setSelectedFile({...selectedFile,tailorid:e.target.value})}}
+          onChange={(e)=>{setSelectedFile({...selectedFile,tailorid:e.target.value})}} style={{marginTop:'20px'}}
         /><br/>
 
-       Enter cost : <input
+       <b>Enter cost</b> <br/><input
           type="number"  id="cost"
           
-          onChange={(e)=>{setSelectedFile({...selectedFile,cost:e.target.value})}}
+          onChange={(e)=>{setSelectedFile({...selectedFile,cost:e.target.value})}}  style={{marginTop:'20px'}}
         /><br/>
 
-        File : <input
+        <b>File</b> <input
           type="file"  id="image"   accept="image/*" 
           
           
-          onChange={(e)=>{setSelectedFile({...selectedFile,file:e.target.files[0]})}}
+          onChange={(e)=>{setSelectedFile({...selectedFile,file:e.target.files[0]})}}  style={{marginTop:'20px'}}
         /><br/>
        
        <button type="submit" className="btn btn-success" style={{marginTop:'20px'}}>Submit</button>
@@ -65,6 +66,5 @@ export default function ImageUpload() {
     </div>
   )
 }
-
 
 
