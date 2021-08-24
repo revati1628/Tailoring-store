ebd2afaf369f417c882dae18086cf9143b546644
revati1628 <88822829+revati1628@users.noreply.github.com>
2021-08-24 19:35:21 +0530

    Update CustomerOrderService.java

--- CustomerOrderService.java
+++ CustomerOrderService.java
@@ -1,27 +1,46 @@
-package com.security.app.service;
+package com.app.project.service;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.security.app.model.AdminModel;
-import com.security.app.model.CustomerOrder;
-import com.security.app.repo.CustomerOrderRepo;
+import com.app.project.dao.OrdersRepository;
+import com.app.project.model.Orders;
+import com.app.project.model.Tailor;
 
 @Service
-public class CustomerOrderService {
+public class OrdersService {
+
+	@Autowired 
+	OrdersRepository repo;
 	
-	@Autowired
-	CustomerOrderRepo repo;
-	
-	public CustomerOrder storeUser(CustomerOrder co) {
+	public Orders storeOrder(Orders co) {
 		repo.save(co);
 		return co;
 	}
 	
-	public List<CustomerOrder> getAll(){
-		List<CustomerOrder> co=repo.findAll();
+	
+	public List<Orders> getAll(){
+		List<Orders> co=repo.findAll();
 		return co;
 	}
+	
+	public Orders getOailorId(int orderid) {
+		Orders orders=repo.findById(orderid).orElse(new Orders());
+		return orders;
+	}
+
+	public List<Orders> getByOrders(int tailorid) {
+		// TODO Auto-generated method stub
+		List<Orders> o=repo.getById(tailorid);
+		return o;
+	}
+	
+	public List ordersByStatus(String approvalstatus,int tailorid) {
+		// TODO Auto-generated method stub
+		List o=repo.getbyStatus(approvalstatus,tailorid);
+		return o;
+	}
 }
+
