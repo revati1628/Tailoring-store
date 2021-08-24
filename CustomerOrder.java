78c4c9c135d93bc36049a8bc800cb9dcdc917ef6
revati1628 <88822829+revati1628@users.noreply.github.com>
2021-08-24 19:32:56 +0530

    Update CustomerOrder.java

--- CustomerOrder.java
+++ CustomerOrder.java
@@ -1,41 +1,49 @@
 package com.app.project.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Orders {
 		@Id
+		@GeneratedValue(strategy = GenerationType.AUTO)
+		private int orderid;
 		private int tailorid;
+		private int customerid;
 		private String dresstype;
 		private String topfabric;
-		private String expectedDateT;
+		//private String expectedDateT;
 		private String topwaist;
 		private String toplength;
 		private String chest;
 		private String shoulderlength;
-		private String topamount;
+		private int topamount;
 		private String neck;
 		private String bottomfabric;
-		private String expectedDateB;
-		private String bottomamount;
+		private String expectedDate;
+		private int bottomamount;
 		private String kneelength;
 		private String hip;
 		private String comments;
+		private String modelid;
+		private String approvalstatus;
 		
 		public Orders() {
 			super();
 		}
 
-		public Orders(int tailorid, String dresstype, String topfabric, String expectedDateT, String topwaist,
-				String toplength, String chest, String shoulderlength, String topamount, String neck,
-				String bottomfabric, String expectedDateB, String bottomamount, String kneelength, String hip,
-				String comments) {
+		public Orders(int orderid, int tailorid, int customerid, String dresstype, String topfabric, String topwaist,
+				String toplength, String chest, String shoulderlength, int topamount, String neck, String bottomfabric,
+				String expectedDate, int bottomamount, String kneelength, String hip, String comments, String modelid,
+				String approvalstatus) {
 			super();
+			this.orderid = orderid;
 			this.tailorid = tailorid;
+			this.customerid = customerid;
 			this.dresstype = dresstype;
 			this.topfabric = topfabric;
-			this.expectedDateT = expectedDateT;
 			this.topwaist = topwaist;
 			this.toplength = toplength;
 			this.chest = chest;
@@ -43,11 +51,21 @@ public class Orders {
 			this.topamount = topamount;
 			this.neck = neck;
 			this.bottomfabric = bottomfabric;
-			this.expectedDateB = expectedDateB;
+			this.expectedDate = expectedDate;
 			this.bottomamount = bottomamount;
 			this.kneelength = kneelength;
 			this.hip = hip;
 			this.comments = comments;
+			this.modelid = modelid;
+			this.approvalstatus = approvalstatus;
+		}
+
+		public int getOrderid() {
+			return orderid;
+		}
+
+		public void setOrderid(int orderid) {
+			this.orderid = orderid;
 		}
 
 		public int getTailorid() {
@@ -56,6 +74,14 @@ public class Orders {
 
 		public void setTailorid(int tailorid) {
 			this.tailorid = tailorid;
+		}
+
+		public int getCustomerid() {
+			return customerid;
+		}
+
+		public void setCustomerid(int customerid) {
+			this.customerid = customerid;
 		}
 
 		public String getDresstype() {
@@ -72,14 +98,6 @@ public class Orders {
 
 		public void setTopfabric(String topfabric) {
 			this.topfabric = topfabric;
-		}
-
-		public String getExpectedDateT() {
-			return expectedDateT;
-		}
-
-		public void setExpectedDateT(String expectedDateT) {
-			this.expectedDateT = expectedDateT;
 		}
 
 		public String getTopwaist() {
@@ -114,11 +132,11 @@ public class Orders {
 			this.shoulderlength = shoulderlength;
 		}
 
-		public String getTopamount() {
+		public int getTopamount() {
 			return topamount;
 		}
 
-		public void setTopamount(String topamount) {
+		public void setTopamount(int topamount) {
 			this.topamount = topamount;
 		}
 
@@ -138,19 +156,19 @@ public class Orders {
 			this.bottomfabric = bottomfabric;
 		}
 
-		public String getExpectedDateB() {
-			return expectedDateB;
+		public String getExpectedDate() {
+			return expectedDate;
 		}
 
-		public void setExpectedDateB(String expectedDateB) {
-			this.expectedDateB = expectedDateB;
+		public void setExpectedDate(String expectedDate) {
+			this.expectedDate = expectedDate;
 		}
 
-		public String getBottomamount() {
+		public int getBottomamount() {
 			return bottomamount;
 		}
 
-		public void setBottomamount(String bottomamount) {
+		public void setBottomamount(int bottomamount) {
 			this.bottomamount = bottomamount;
 		}
 
@@ -178,20 +196,34 @@ public class Orders {
 			this.comments = comments;
 		}
 
+		public String getModelid() {
+			return modelid;
+		}
+
+		public void setModelid(String modelid) {
+			this.modelid = modelid;
+		}
+
+		public String getApprovalstatus() {
+			return approvalstatus;
+		}
+
+		public void setApprovalstatus(String approvalstatus) {
+			this.approvalstatus = approvalstatus;
+		}
+
 		@Override
 		public String toString() {
-			return "Orders [tailorid=" + tailorid + ", dresstype=" + dresstype + ", topfabric=" + topfabric
-					+ ", expectedDateT=" + expectedDateT + ", topwaist=" + topwaist + ", toplength=" + toplength
-					+ ", chest=" + chest + ", shoulderlength=" + shoulderlength + ", topamount=" + topamount + ", neck="
-					+ neck + ", bottomfabric=" + bottomfabric + ", expectedDateB=" + expectedDateB + ", bottomamount="
-					+ bottomamount + ", kneelength=" + kneelength + ", hip=" + hip + ", comments=" + comments + "]";
+			return "Orders [orderid=" + orderid + ", tailorid=" + tailorid + ", customerid=" + customerid
+					+ ", dresstype=" + dresstype + ", topfabric=" + topfabric + ", topwaist=" + topwaist
+					+ ", toplength=" + toplength + ", chest=" + chest + ", shoulderlength=" + shoulderlength
+					+ ", topamount=" + topamount + ", neck=" + neck + ", bottomfabric=" + bottomfabric
+					+ ", expectedDate=" + expectedDate + ", bottomamount=" + bottomamount + ", kneelength=" + kneelength
+					+ ", hip=" + hip + ", comments=" + comments + ", modelid=" + modelid + ", approvalstatus="
+					+ approvalstatus + "]";
 		}
-		
-		
 
-		
 		
 		
 	}
-
 
